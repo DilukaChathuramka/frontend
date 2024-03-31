c0dd6601471afc4f2af0f82c6688e0eb6e80162c
Diluka Chathuramka <159243058+DilukaChathuramka@users.noreply.github.com>
2024-03-31 22:24:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,35 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
+import Layout from "./pages/Layout.jsx";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Feature from "./components/Feature";
 import GetStart from "./components/GetStart";
 import Footer from "./components/Footer";
-import Packages from "./pages/Packages";
+import Dashboard from "./pages/dashboard/Dashboard.jsx";
+import Cards from "./pages/dashboard/Cards";
 import Login from "./pages/Login";
 import Signin from "./pages/Signin";
-import CustommizePackage from "./pages/CustommizePackage";
-import Dashboard from "./pages/dashboard/Dashboard";
-
-import Cards from "./pages/dashboard/Cards";
-import DriverRegistration from "./pages/dashboard/driver/DriverRegistration";
-import Addvehicle from "./pages/dashboard/vehicle/Addvehicle";
-import UserDetails from "./pages/dashboard/user/UserDetails";
-import UserProfile from "./components/UserProfile";
-import Verify from "./pages/Verify";
 import axios from "axios";
-import { UserProvider } from "./context/UserContext";
-import Fedback from "./pages/Fedback";
-import PackageAddForm from "./pages/dashboard/packages/PackageAddForm";
-import Payment from "./pages/Payment";
-import FeedbackFrom from "./pages/FeedbackFrom";
-import DriverManagement from "./pages/dashboard/driver/DriverManagement";
-
+import Verify from "./pages/Verify";
 axios.defaults.baseURL = "http://localhost:5000";
 axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <>
-      <UserProvider>
-        <Routes>
-          <Route
-            path="/"
+      
+      <Routes>
+        <Route path="/" element={<><Header/><Home /><Feature /><GetStart /><Footer/></>} />
+         <Route
+            path="/dashboard"
             element={
-              <>
-                <Header />
-                <Home />
-                <Feature />
-                <GetStart />
-                <Footer />
-              </>
+              <Dashboard>
+                <Cards />
+              </Dashboard>
             }
           />
-          <Route
-            path="/packages"
-            element={
-              <Layout>
-                <Packages />
-              </Layout>
-            }
-          />
-          <Route
+<Route
             path="/login"
             element={
               <Layout>
@@ -71,70 +46,6 @@ function App() {
             }
           />
           <Route
-            path="/cutomizepackages"
-            element={
-              <Layout>
-                <CustommizePackage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <Dashboard>
-                <Cards />
-              </Dashboard>
-            }
-          />
-          <Route
-            path="/driver-registration"
-            element={
-              <Dashboard>
-                <DriverRegistration />
-              </Dashboard>
-            }
-          />
-          <Route
-            path="/vehicle-add"
-            element={
-              <Dashboard>
-                <Addvehicle />
-              </Dashboard>
-            }
-          />
-           <Route
-            path="/allvehicle"
-            element={
-              <Dashboard>
-                <DriverManagement/>
-              </Dashboard>
-            }
-          />
-          <Route
-            path="/user-details"
-            element={
-              <Dashboard>
-                <UserDetails />
-              </Dashboard>
-            }
-          />
-             <Route
-            path="/dashPackage"
-            element={
-              <Dashboard>
-                <PackageAddForm/>
-              </Dashboard>
-            }
-          />
-          <Route
-            path="/user-profile"
-            element={
-              <Layout>
-                <UserProfile />
-              </Layout>
-            }
-          />
-          <Route
             path="/user/:id/verify/:token"
             element={
               <Layout>
@@ -142,33 +53,9 @@ function App() {
               </Layout>
             }
           />
-            <Route
-            path="/feedback"
-            element={
-              <Layout>
-                <Fedback/>
-              </Layout>
-            }
-          />
-           <Route
-            path="/feedback-form"
-            element={
-              <Layout>
-                <FeedbackFrom/>
-              </Layout>
-            }
-          />
-             <Route
-            path="/payment"
-            element={
-              <Layout>
-                <Payment/>
-              </Layout>
-            }
-          />
-          {/* <Route path="/verify" element={<Verify/>}/> */}
-        </Routes>
-      </UserProvider>
+      </Routes>
+
+   
     </>
   );
 }
